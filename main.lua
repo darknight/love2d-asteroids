14cf8a16bd097491a4d6f5ac8914a7d17895b6aa
darknight <illuminating.me@gmail.com>
2013-12-31 00:31:11 +0800

    version 3, basic collision

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 player_ship = {} -- the player
 score_label = "Score: 0"
-level_label = "Version 2: Basic Motion"
+level_label = "Version 3: Basic Collision"
 
 love.math.setRandomSeed(os.time())
 
@@ -30,6 +30,12 @@ function check_bounds(obj)
     end
 end
 
+function collide(obj1, obj2)
+    collision_dist = obj1.width / 2 + obj2.width / 2
+    actual_dist = distance(obj1.x, obj1.y, obj2.x, obj2.y)
+    return actual_dist <= collision_dist
+end
+
 function update_obj(obj, dt)
     obj.x = obj.x + obj.velocity_x * dt
     obj.y = obj.y + obj.velocity_y * dt
@@ -51,6 +57,7 @@ function load_asteroids(num_asteroids, player_x, player_y)
         asteroid.height = asteroid.image:getHeight()
         asteroid.velocity_x = love.math.random() * 40
         asteroid.velocity_y = love.math.random() * 40
+        asteroid.dead = false
         table.insert(asteroids, asteroid)
     end
     return asteroids
@@ -72,23 +79,34 @@ function load_player()
     player.thrust = 300
     player.rotate_speed = 200
     player.rotation = 0
+    player.dead = false
     return player
 end
 
-function update_player(player, dt)
+function update_player(dt)
+    local player = player_ship
     update_obj(player, dt)
+
+    if love.keyboard.isDown("left") then
+        player.rotation = player.rotation - player.rotate_speed * dt
+    end
+    if love.keyboard.isDown("right") then
+        player.rotation = player.rotation + player.rotate_speed * dt
+    end
+
     if love.keyboard.isDown("up") then
         angle_radians = math.rad(player.rotation)
         force_x = math.cos(angle_radians) * player.thrust * dt
         force_y = math.sin(angle_radians) * player.thrust * dt
         player.velocity_x = player.velocity_x + force_x
         player.velocity_y = player.velocity_y + force_y
-    end
-    if love.keyboard.isDown("left") then
-        player.rotation = player.rotation - player.rotate_speed * dt
-    end
-    if love.keyboard.isDown("right") then
-        player.rotation = player.rotation + player.rotate_speed * dt
+        -- update engine sprite
+        engine_sprite.rotation = player.rotation
+    engine_sprite.x = player.x
+    engine_sprite.y = player.y
+    engine_sprite.visible = true
+    else
+        engine_sprite.visible = false
     end
 end
 
@@ -100,6 +118,18 @@ function load_player_lives(num_icons)
     end
     return sprite_batch
 end
+
+function load_engine_flame()
+    local engine = {}
+    engine.image = love.graphics.newImage("resources/engine_flame.png")
+    engine.x = 0
+    engine.y = 0
+    engine.rotation = 0
+    engine.visible = false
+    engine.width = engine.image:getWidth()
+    engine.height = engine.image:getHeight()
+    return engine
+end
 -----------------------love function-----------------
 
 function love.load()
@@ -108,6 +138,7 @@ function love.load()
     player_ship = load_player()
     asteroids = load_asteroids(3, player_ship.x, player_ship.y)
     player_lives = load_player_lives(2)
+    engine_sprite = load_engine_flame()
 end
 
 function love.draw()
@@ -115,20 +146,63 @@ function love.draw()
     local level_width = main_font:getWidth(level_label)
     love.graphics.print(score_label, 10, 25)
     love.graphics.printf(level_label, 400, 25, level_width, "left", 0, 1, 1, level_width / 2, 0, 0, 0)
-    -- draw player ship
-    love.graphics.draw(player_ship.image, player_ship.x, player_ship.y, math.rad(player_ship.rotation), 1, 1, 
-                       player_ship.width / 2, player_ship.height / 2)
+
+    -- draw player ship and engine flame
+    if player_ship then
+        love.graphics.draw(player_ship.image, player_ship.x, player_ship.y, math.rad(player_ship.rotation),
+                           1, 1, player_ship.width / 2, player_ship.height / 2)
+        if engine_sprite.visible then
+            love.graphics.draw(engine_sprite.image, engine_sprite.x, engine_sprite.y,
+                               math.rad(engine_sprite.rotation), 1, 1, engine_sprite.width * 1.5,
+                               engine_sprite.height / 2)
+        end
+    end
+
     -- draw asteroids
     for _, v in ipairs(asteroids) do
         love.graphics.draw(v.image, v.x, v.y, math.rad(v.rotation), 1, 1, v.width / 2, v.height / 2)
     end
+
     -- draw player lives in sprite batch
     love.graphics.draw(player_lives)
 end
 
 function love.update(dt)
-    update_player(player_ship, dt)
+    if player_ship then
+        update_player(dt)
+    end
     for _, v in ipairs(asteroids) do
         update_asteroid(v, dt)
     end
+
+    -- collision detection
+    for i, v in ipairs(asteroids) do
+        obj_1 = asteroids[i]
+        if player_ship and not player_ship.dead and not obj_1.dead then
+            if collide(obj_1, player_ship) then
+                obj_1.dead = true
+                player_ship.dead = true
+            end
+        end
+        for j = i+1, #asteroids do
+            obj_2 = asteroids[j]
+            if not obj_1.dead and not obj_2.dead then
+                if collide(obj_1, obj_2) then
+                    obj_1.dead = true
+                    obj_2.dead = true
+                end
+            end
+        end
+    end
+    -- remove dead objects
+    if player_ship and player_ship.dead then
+        player_ship = nil
+    end
+    local temp_asteroids = {}
+    for _, v in ipairs(asteroids) do
+        if not v.dead then
+            table.insert(temp_asteroids, v)
+        end
+    end
+    asteroids = temp_asteroids
 end
